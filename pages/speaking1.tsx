6bb189bb4565b3e18d75d701df60af2cb2b070b8
Alex <alexander.crump11@gmail.com>
2023-01-26 17:29:42 +0100

    [Speak1]: logic amended to enable theme in view

--- pages/speaking1.tsx
+++ pages/speaking1.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Questionbtn from '../components/questionbtn';
 import { Part2QStructure, Part3QStructure } from '../types/types';
 import { part1 } from '../dataPart1';
-import styles from '../styles/speaking3.module.css';
+import styles from '../styles/speaking1.module.css';
 import Timer from '../components/timer';
 import Instructions from '../components/instructions';
 
@@ -10,21 +10,22 @@ export default function SpeakingOne() {
   const [question, setQuestion] = useState<
     string | Part2QStructure | Part3QStructure
   >();
+  const [theme, setTheme] = useState<string>();
   const [questionNum, setQuestionNum] = useState<number>();
 
-  const questionsPart1 = part1.questionsByTheme
-    .map((val) => val.questions)
-    .flat();
+  const themes = part1.questionsByTheme;
 
   const handleSelectQuestion = () => {
-    console.log(questionsPart1);
-    let i = parseFloat(
-      (Math.random() * (questionsPart1.length - 1)).toFixed(0)
+    let i = parseFloat((Math.random() * (themes.length - 1)).toFixed(0));
+    let j = parseFloat(
+      (Math.random() * (themes[i].questions.length - 1)).toFixed(0)
     );
 
-    if (i !== questionNum) {
-      setQuestion(questionsPart1[i]);
-      setQuestionNum(i);
+    if (j !== questionNum) {
+      setTheme(themes[i].theme);
+      setQuestion(themes[i].questions[j]);
+      setQuestionNum(j);
+      console.log(theme, question);
       return;
     }
     handleSelectQuestion();
@@ -38,6 +39,7 @@ export default function SpeakingOne() {
         <>
           {question && typeof question === 'string' ? (
             <div className={`${styles.themeCont} glass`}>
+              <p className={styles.themeText}>{theme}</p>
               <h2>{question}</h2>
             </div>
           ) : (
