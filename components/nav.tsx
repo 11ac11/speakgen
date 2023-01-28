bbcf29a80d04d3e1b63c1e1a898d02fe3624fa14
Alex <alexander.crump11@gmail.com>
2023-01-28 14:18:00 +0100

    [CSS]: Amended nav for wider screens, speaking 3 for a more uniformed layout across devices

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,27 +4,29 @@ import Link from 'next/link';
 export default function Nav() {
   return (
     <div className={`${styles.navbar} glass`}>
-      <div className={styles.leftNav}>
-        <h1>First Certificate Speaking</h1>
-      </div>
-      <div className={styles.rightNav}>
-        <ul>
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/speaking1">Part 1</Link>
-          </li>
-          <li>
-            <Link href="/speaking2">Part 2</Link>
-          </li>
-          <li>
-            <Link href="/speaking3">Part 3</Link>
-          </li>
-          <li>
-            <Link href="/speaking4">Part 4</Link>
-          </li>
-        </ul>
+      <div className={styles.navCont}>
+        <div className={styles.leftNav}>
+          <h1>First Certificate Speaking</h1>
+        </div>
+        <div className={styles.rightNav}>
+          <ul>
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/speaking1">Part 1</Link>
+            </li>
+            <li>
+              <Link href="/speaking2">Part 2</Link>
+            </li>
+            <li>
+              <Link href="/speaking3">Part 3</Link>
+            </li>
+            <li>
+              <Link href="/speaking4">Part 4</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
