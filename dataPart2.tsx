4204b0c23971b6e521e8c085e1018ed48abd8bce
Alex <alexander.crump11@gmail.com>
2023-02-07 20:15:42 +0100

    [Speaking2]: added more questions

--- dataPart2.tsx
+++ dataPart2.tsx
@@ -73,5 +73,72 @@ export const part2: Part2 = {
         },
       ],
     },
+    {
+      theme: 'People & Life',
+      questions: [
+        {
+          statement: 'How important is it to help in these situations?',
+          image1:
+            'https://images.unsplash.com/photo-1609558931141-606ae4284153?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+          image2:
+            'https://www.football4football.com/storage/img/articleimages/originals/I4wFD9e3NTkze9DSIuc33gS6HyHXP4OHgkl.jpg',
+          followUp: 'Can you think of a time when you helped someone?',
+        },
+        {
+          statement:
+            'What are the people enjoying about spending time in these gardens?',
+          image1:
+            'https://images.unsplash.com/photo-1601001815894-4bb6c81416d7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+          image2:
+            'https://images.unsplash.com/photo-1570543593818-addd35a926a5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=435&q=80',
+          followUp: 'Have you ever tried to grow any food from a plant?',
+        },
+      ],
+    },
+    {
+      theme: 'Technology',
+      questions: [
+        {
+          statement:
+            'Which of these inventions have affected our lives most and why?',
+          image1:
+            'https://images.unsplash.com/photo-1606768666853-403c90a981ad?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
+          image2:
+            'https://images.unsplash.com/photo-1483478550801-ceba5fe50e8e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+          followUp: 'Do you use a phone or a computer more frequently?',
+        },
+        {
+          statement: 'How has technology helped improve these industries?',
+          image1:
+            'https://images.unsplash.com/photo-1481555716071-8830d3e254ba?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+          image2:
+            'https://plus.unsplash.com/premium_photo-1663039952394-00e73f235728?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+          followUp:
+            'Can you think of another industry that uses lots of new technology?',
+        },
+      ],
+    },
+    {
+      theme: 'Entertainment',
+      questions: [
+        {
+          statement: 'Why might people enjoy listening to music in these ways?',
+          image1:
+            'https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+          image2:
+            'https://images.unsplash.com/photo-1519121664077-7a7c5beef683?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=786&q=80',
+          followUp: 'How do you like to listen to music?',
+        },
+        {
+          statement:
+            'Why might people prefer to watch something in one of these ways more than the other?',
+          image1:
+            'https://images.unsplash.com/photo-1608170825938-a8ea0305d46c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=725&q=80',
+          image2:
+            'https://images.unsplash.com/photo-1585699324551-f6c309eedeca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
+          followUp: 'Do you enjoy going to the theatre?',
+        },
+      ],
+    },
   ],
 };
