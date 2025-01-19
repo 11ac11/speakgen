2d91a8edd6f416b59bddb46ac8a0df2874ca2301
11ac11 <alexander.crump11@gmail.com>
2025-01-19 20:20:05 +0100

    fixed home page

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,10 +2,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <>
-      <div>
-        <p>Welcome to First Certificate Speaking practise</p>
-      </div>
-    </>
+    <div>
+      <p>Welcome to First Certificate Speaking practise</p>
+    </div>
   );
 }
