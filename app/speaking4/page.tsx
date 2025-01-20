b3e2949ba40d0c8af4c404250249b7b5d93a1fa4
11ac11 <alexander.crump11@gmail.com>
2025-01-20 17:08:34 +0100

    connection made with RDS

--- app/speaking4/page.tsx
+++ app/speaking4/page.tsx
@@ -1,7 +1,16 @@
 import React from "react";
 import QuestionContainer from "../../components/QuestionContainer";
+import ConnectionTest from "./connectionTest";
 import { part4data } from "../../dataPart4";
+import { GET } from "../api/aws-rds/route";
 
-export default function SpeakingFour() {
-  return <QuestionContainer questions={part4data} />;
+export default async function SpeakingFour() {
+  const result = await GET();
+  console.log("result:", result);
+  return (
+    <>
+      <ConnectionTest />
+      <QuestionContainer questions={part4data} />
+    </>
+  );
 }
