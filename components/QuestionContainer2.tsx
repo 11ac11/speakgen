acf2d4672bccaf42110e2337ff03791c9ccd6c35
11ac11 <alexander.crump11@gmail.com>
2025-03-09 17:12:27 +0100

    added loading spinner

--- components/QuestionContainer2.tsx
+++ components/QuestionContainer2.tsx
@@ -6,13 +6,22 @@ import Button from "./ui/Button";
 import Instructions from "./Instructions";
 import PartOneQuestion from "./PartOneQuestion";
 import Timer from "./Timer";
+import { LoadingSpinner } from "./ui/LoadingSpinner";
 
 export default function QuestionContainer() {
   const [question, setQuestion] = useState<any | null>(null);
+  const [loading, setLoading] = useState<boolean>(false);
 
   async function fetchNextQuestion() {
-    const newQuestion = await getRandomPartOneQuestion();
-    setQuestion(newQuestion);
+    try {
+      setLoading(true);
+      const newQuestion = await getRandomPartOneQuestion();
+      setQuestion(newQuestion);
+    } catch (e) {
+      console.error(e);
+    } finally {
+      setLoading(false);
+    }
   }
 
   return (
@@ -30,6 +39,7 @@ export default function QuestionContainer() {
         </div>
         {question && <Timer question={question} timeLeft={60} />}
       </div>
+      {loading && <LoadingSpinner />}
       {question ? (
         <PartOneQuestion
           question={question.question}
