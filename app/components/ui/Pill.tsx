59e12aec43499f7b583f97d42cd4407b25808d89
11ac11 <alexander.crump11@gmail.com>
2025-04-02 08:47:43 +0200

    updated Pill component

--- app/components/ui/Pill.tsx
+++ app/components/ui/Pill.tsx
@@ -1,55 +1,6 @@
 import { useMemo } from "react";
 import styled, { css } from "styled-components";
 
-export default function Pill({
-  className,
-  bgColor,
-  textColor,
-  text,
-  onClick,
-  showRemove,
-  style,
-}: {
-  className?: string;
-  bgColor?: string;
-  textColor?: string;
-  text: string;
-  onClick?: () => void;
-  showRemove?: boolean;
-  style?: any; // todo change
-}) {
-  const backgroundColor = bgColor
-    ? bgColor
-    : useMemo(() => generateColor(text, false, true), [text]);
-  const _textColor = textColor
-    ? textColor
-    : useMemo(() => generateColor(text, false, false), [text]);
-
-  return (
-    <StyledPill
-      style={{ ...style, backgroundColor, color: _textColor }}
-      onClick={onClick}
-      className={className}
-      $showHoverEffect={!!onClick}
-    >
-      <TextSection>{text}</TextSection>
-      {showRemove && (
-        <RemoveBox style={{ borderLeft: `1px solid ${_textColor}` }}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            fill="currentColor"
-            width="20"
-            height="20"
-          >
-            <path d="M18.3 5.7a1 1 0 0 0-1.4 0L12 10.59 7.1 5.7a1 1 0 1 0-1.4 1.42L10.59 12l-4.9 4.88a1 1 0 1 0 1.42 1.42L12 13.41l4.88 4.89a1 1 0 0 0 1.42-1.42L13.41 12l4.89-4.88a1 1 0 0 0 0-1.42z" />
-          </svg>
-        </RemoveBox>
-      )}
-    </StyledPill>
-  );
-}
-
 const TextSection = styled.div`
   padding: 4px 6px;
 `;
@@ -87,33 +38,64 @@ const StyledPill = styled.div<{ $showHoverEffect: boolean }>`
   `}
 `;
 
-// Predefined colors for specific parts
-const PREDEFINED_COLORS: { [key: string]: { bg: string; text: string } } = {
-  "part 1": { bg: "#b3efb2", text: "#1f3b1e" },
-  "part 2": { bg: "#bde69a", text: "#dff3e2" },
-  "part 3": { bg: "#93ce60", text: "#182720" },
-  "part 4": { bg: "#7a9e7e", text: "#1e2b20" },
-};
+export default function Pill({
+  className,
+  bgColor,
+  textColor,
+  text,
+  onClick,
+  showRemove,
+  style,
+}: {
+  className?: string;
+  bgColor?: string;
+  textColor?: string;
+  text: string;
+  onClick?: () => void;
+  showRemove?: boolean;
+  style?: any; // todo change
+}) {
+  const backgroundColor = bgColor
+    ? bgColor
+    : useMemo(() => generateColor(text, true), [text]);
+  const _textColor = textColor
+    ? textColor
+    : useMemo(() => generateColor(text, false), [text]);
+
+  return (
+    <StyledPill
+      style={{ ...style, backgroundColor, color: _textColor }}
+      onClick={onClick}
+      className={className}
+      $showHoverEffect={!!onClick}
+    >
+      <TextSection>{text}</TextSection>
+      {showRemove && (
+        <RemoveBox style={{ borderLeft: `1px solid ${_textColor}` }}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+            width="20"
+            height="20"
+          >
+            <path d="M18.3 5.7a1 1 0 0 0-1.4 0L12 10.59 7.1 5.7a1 1 0 1 0-1.4 1.42L10.59 12l-4.9 4.88a1 1 0 1 0 1.42 1.42L12 13.41l4.88 4.89a1 1 0 0 0 1.42-1.42L13.41 12l4.89-4.88a1 1 0 0 0 0-1.42z" />
+          </svg>
+        </RemoveBox>
+      )}
+    </StyledPill>
+  );
+}
 
 // Function to generate color based on text
-function generateColor(text: string, useLightPalette: boolean, forBg: boolean) {
+function generateColor(text: string, forBg: boolean) {
   const normalizedText = text.trim().toLowerCase();
 
-  if (PREDEFINED_COLORS[normalizedText]) {
-    return forBg
-      ? PREDEFINED_COLORS[normalizedText].bg
-      : PREDEFINED_COLORS[normalizedText].text;
-  }
-
-  return generateColorFromText(text, useLightPalette, forBg);
+  return generateColorFromText(text, forBg);
 }
 
 // Function to generate color from text
-function generateColorFromText(
-  text: string,
-  useLightPalette: boolean,
-  forBg: boolean
-) {
+function generateColorFromText(text: string, forBg: boolean) {
   if (text.length < 2 || BASE_COLORS.length === 0) return "#ccc"; // Fallback color
 
   const char1 = text.charCodeAt(0);
@@ -121,13 +103,6 @@ function generateColorFromText(
   const index = (char1 + char2) % BASE_COLORS.length;
 
   let [r, g, b] = forBg ? BASE_COLORS[index].bg : BASE_COLORS[index].text;
-
-  // Apply pastel effect if needed (mix with white)
-  if (useLightPalette) {
-    r = Math.round(r + (255 - r) * 0.5);
-    g = Math.round(g + (255 - g) * 0.5);
-    b = Math.round(b + (255 - b) * 0.5);
-  }
 
   return `rgb(${r}, ${g}, ${b})`;
 }
