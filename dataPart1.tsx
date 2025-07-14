24abb6511364cdf8e6872f772cb088b59594a8a8
11ac11 <alexander.crump11@gmail.com>
2025-07-14 16:26:41 +0200

    commented default questions

--- dataPart1.tsx
+++ dataPart1.tsx
@@ -1,74 +1,74 @@
-import { Part1and4 } from "./types/types";
+// import { Part1and4 } from "./types/types";
 
-export const part1data: Part1and4 = {
-  instructions:
-    "The examiner will ask your name and then roughly 3-6 of these types of questions. They will be from a variety of topics.",
-  time: 15,
-  part: "part1/4",
-  speakTo: "The examiner",
-  questionsByTheme: [
-    {
-      theme: "Your country",
-      questions: [
-        "Is there anything you would like to learn about your country?",
-        "Which area of your country would you like to get to know better?",
-      ],
-    },
-    {
-      theme: "Daily Life",
-      questions: [
-        "Tell us about a day you've really enjoyed recently.",
-        "Are you planning to do anything special this weekend?",
-      ],
-    },
-    {
-      theme: "Sports",
-      questions: [
-        "Tell us about an unusal sport someone you know likes",
-        "What is the most popular sport where you live?",
-      ],
-    },
-    {
-      theme: "Fashion",
-      questions: [
-        "What type of clothes do you like to wear?",
-        "What are your favourite clothes shops?",
-      ],
-    },
-    {
-      theme: "Environment",
-      questions: [
-        "What do you do to help the environment?",
-        "How important is saving the environment to you?",
-      ],
-    },
-    {
-      theme: "Family",
-      questions: [
-        "Describe a friend or family member who you look up to",
-        "Do you prefer to spend time with your friends or family?",
-      ],
-    },
-    {
-      theme: "Food",
-      questions: [
-        "Tell us about a strange food you have tried",
-        "What is your perfect meal?",
-      ],
-    },
-    {
-      theme: "Education",
-      questions: [
-        "Do you learn more from listening/reading or by doing?",
-        "Tell us about your school/college/university",
-      ],
-    },
-    {
-      theme: "TV/Film",
-      questions: [
-        "What is your favourite genre of movie?",
-        "How often do you watch TV/series?",
-      ],
-    },
-  ],
-};
+// export const part1data: Part1and4 = {
+//   instructions:
+//     "The examiner will ask your name and then roughly 3-6 of these types of questions. They will be from a variety of topics.",
+//   time: 15,
+//   part: "part1/4",
+//   speakTo: "The examiner",
+//   questionsByTheme: [
+//     {
+//       theme: "Your country",
+//       questions: [
+//         "Is there anything you would like to learn about your country?",
+//         "Which area of your country would you like to get to know better?",
+//       ],
+//     },
+//     {
+//       theme: "Daily Life",
+//       questions: [
+//         "Tell us about a day you've really enjoyed recently.",
+//         "Are you planning to do anything special this weekend?",
+//       ],
+//     },
+//     {
+//       theme: "Sports",
+//       questions: [
+//         "Tell us about an unusal sport someone you know likes",
+//         "What is the most popular sport where you live?",
+//       ],
+//     },
+//     {
+//       theme: "Fashion",
+//       questions: [
+//         "What type of clothes do you like to wear?",
+//         "What are your favourite clothes shops?",
+//       ],
+//     },
+//     {
+//       theme: "Environment",
+//       questions: [
+//         "What do you do to help the environment?",
+//         "How important is saving the environment to you?",
+//       ],
+//     },
+//     {
+//       theme: "Family",
+//       questions: [
+//         "Describe a friend or family member who you look up to",
+//         "Do you prefer to spend time with your friends or family?",
+//       ],
+//     },
+//     {
+//       theme: "Food",
+//       questions: [
+//         "Tell us about a strange food you have tried",
+//         "What is your perfect meal?",
+//       ],
+//     },
+//     {
+//       theme: "Education",
+//       questions: [
+//         "Do you learn more from listening/reading or by doing?",
+//         "Tell us about your school/college/university",
+//       ],
+//     },
+//     {
+//       theme: "TV/Film",
+//       questions: [
+//         "What is your favourite genre of movie?",
+//         "How often do you watch TV/series?",
+//       ],
+//     },
+//   ],
+// };
--- dataPart4.tsx
+++ dataPart4.tsx
@@ -1,52 +1,52 @@
-import { Part1and4 } from "./types/types";
+// import { Part1and4 } from "./types/types";
 
-export const part4data: Part1and4 = {
-  instructions:
-    "The examiner will ask you questions related to the topic from part 3. You should answer the questions, but also ask your partners opinions and ideas. It should be a conversation between 3 people.",
-  time: 60,
-  part: "part1/4",
-  speakTo: "The examiner and your partner",
-  questionsByTheme: [
-    {
-      theme: "Travel",
-      questions: [
-        "Do you think you have to spend a lot of money to have a good holiday?",
-        "Some people say we travel too much these days and shouldn't go on so many holidays. What do you think?",
-        "Why do you think people like to go away on holiday?",
-        "What do you think is the biggest advantage of living in a place where there are a lot of tourists?",
-      ],
-    },
-    {
-      theme: "Where you live",
-      questions: [
-        "What can people do to have a good holiday in your country?",
-        "What's good about living in cities in your country?",
-        "Which is the best city for people to visit in your country?",
-        "If you could choose to visit a city you've never been to, which one would you choose?",
-        "Would you prefer to live in a modern city or a city with lots of history?",
-        "Are there advantages to living in a small town rather than in a big city?",
-        "Do you think it is better for children to grow up in the city or in the countryside?",
-      ],
-    },
-    {
-      theme: "History",
-      questions: [
-        "What do you think makes a good museum?",
-        "How do you think the teaching of history in schools could be improved?",
-        "Do you agree that learning about our past is important for our future?",
-        "What has been the most important moment in your life so far?",
-        "What items from our lives today will be in the history museums of the future?",
-      ],
-    },
-    {
-      theme: "Life & Leisure",
-      questions: [
-        "If you could change one thing about your life, what would you change?",
-        "Many people say life's too busy these days. Why do you think they say this?",
-        "Many people want to become famous nowadays. Why do you think this is?",
-        "Is it important to enjoy a job or do you think it's enough to be paid well?",
-        "How important is it to go on holiday every year?",
-      ],
-    },
-  ],
-};
+// export const part4data: Part1and4 = {
+//   instructions:
+//     "The examiner will ask you questions related to the topic from part 3. You should answer the questions, but also ask your partners opinions and ideas. It should be a conversation between 3 people.",
+//   time: 60,
+//   part: "part1/4",
+//   speakTo: "The examiner and your partner",
+//   questionsByTheme: [
+//     {
+//       theme: "Travel",
+//       questions: [
+//         "Do you think you have to spend a lot of money to have a good holiday?",
+//         "Some people say we travel too much these days and shouldn't go on so many holidays. What do you think?",
+//         "Why do you think people like to go away on holiday?",
+//         "What do you think is the biggest advantage of living in a place where there are a lot of tourists?",
+//       ],
+//     },
+//     {
+//       theme: "Where you live",
+//       questions: [
+//         "What can people do to have a good holiday in your country?",
+//         "What's good about living in cities in your country?",
+//         "Which is the best city for people to visit in your country?",
+//         "If you could choose to visit a city you've never been to, which one would you choose?",
+//         "Would you prefer to live in a modern city or a city with lots of history?",
+//         "Are there advantages to living in a small town rather than in a big city?",
+//         "Do you think it is better for children to grow up in the city or in the countryside?",
+//       ],
+//     },
+//     {
+//       theme: "History",
+//       questions: [
+//         "What do you think makes a good museum?",
+//         "How do you think the teaching of history in schools could be improved?",
+//         "Do you agree that learning about our past is important for our future?",
+//         "What has been the most important moment in your life so far?",
+//         "What items from our lives today will be in the history museums of the future?",
+//       ],
+//     },
+//     {
+//       theme: "Life & Leisure",
+//       questions: [
+//         "If you could change one thing about your life, what would you change?",
+//         "Many people say life's too busy these days. Why do you think they say this?",
+//         "Many people want to become famous nowadays. Why do you think this is?",
+//         "Is it important to enjoy a job or do you think it's enough to be paid well?",
+//         "How important is it to go on holiday every year?",
+//       ],
+//     },
+//   ],
+// };
